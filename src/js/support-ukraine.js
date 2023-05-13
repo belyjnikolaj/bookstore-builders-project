b6dcfc68d07e2ee19f1d3f7f96390086e64e7bd2
vitaliyzeykan <112661803+vitaliyzeykan@users.noreply.github.com>
2023-05-13 22:28:40 +0300

    change some file

--- src/js/support-ukraine.js
+++ src/js/support-ukraine.js
@@ -1,9 +1,11 @@
-import Swiper from 'swiper';
-import 'swiper/css';
+import Swiper from 'swiper/swiper-bundle';
+import 'swiper/swiper-bundle.min.css';
 
-const swiper = new Swiper('.swiper', {
-    
-
+const swiper = new Swiper('.support-list', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
 });
 
 export { supportUkraine };
