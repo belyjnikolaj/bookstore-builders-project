4262f2aef1e01961a7bf226dce3322c76b57b30a
belyjnikolaj <117487574+belyjnikolaj@users.noreply.github.com>
2023-05-16 00:11:31 +0300

    add
    add

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { fetchBestSellers } from './js/best-selling-books';
 import { supportUkraine } from './js/support-ukraine';
 import { modalCard } from './js/modal-card';
-import { fetchBestSellers } from './js/best-selling-books';
-
+import { fetchCategories } from './js/categories-list-list';
 supportUkraine();
 createHero();
+fetchCategories();
 fetchBestSellers()
-  .then(data => bestSellersGal.insertAdjacentHTML('beforeend', createMarkupBooksCategories(data)))
-  .catch(err => console.log(err));
+//   .then(data => bestSellersGal.insertAdjacentHTML('beforeend', createMarkupBooksCategories(data)))
+//   .catch(err => console.log(err));
 
