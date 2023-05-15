441d1e7f0d2716e277f2300a9606214f706d2443
viktoriia-2002 <92129770+viktoriia-2002@users.noreply.github.com>
2023-05-15 15:51:40 -0400

    modal-window

--- src/js/modal-card.js
+++ src/js/modal-card.js
@@ -83,11 +83,7 @@ const renderBooks = (data, refs) => {
   refs.modalCard.insertAdjacentHTML('beforeend', bookElMarkup);
 };
 
-//  <a href="https://www.linkedin.com/in/nataliia-valko-951501212/" target="_blank" class="team-linkd">
-//     <svg width="24" height="24">
-//         <use href="${svgLinkdUrl}#icon-linkedin-svg"></use>
-//      </svg>
-//   </a>
+
 
 document.addEventListener('keydown', function (event) {
   if (event.key === 'Escape') {
