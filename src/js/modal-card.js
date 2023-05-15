2f1cb619ed05c219462bfc86aabb5e5d7f4f4816
viktoriia-2002 <92129770+viktoriia-2002@users.noreply.github.com>
2023-05-14 22:12:21 -0400

    working on a stores

--- src/js/modal-card.js
+++ src/js/modal-card.js
@@ -1,84 +1,3 @@
-// import axios from 'axios';
-// import { BookAPI } from './booksApi';
-// console.log({ BookAPI });
-
-// const refs = {
-//   openModalCardBtn: document.querySelector(
-//     'button[name="data-modal-card-open"]'
-//   ),
-//   modalCard: document.querySelector('#data-modal-card'),
-//   closeModalCardBtn: document.querySelector('.modal-card_close'),
-//   modalCardForm: document.querySelector('.modal-card-form'),
-// };
-
-// refs.openModalCardBtn.addEventListener('click', openModalCard);
-// refs.closeModalCardBtn.addEventListener('click', closeModalCard);
-
-// let isModalOpen = false;
-
-// function openModalCard() {
-//   if (!isModalOpen) {
-//     toggleModal();
-//     fetchBook();
-//     isModalOpen = true;
-//   }
-// }
-
-// function closeModalCard() {
-//   toggleModal();
-//   clearModalContent();
-// }
-
-// function toggleModal() {
-//   document.body.classList.toggle('modal-open');
-//   refs.modalCard.classList.toggle('is-hidden');
-// }
-
-// function clearModalContent() {
-//   refs.modalCard.innerHTML = '';
-// }
-
-// const renderBooks = (data, refs) => {
-//   const amazonLink = data.buy_links.find(link => link.name === 'Amazon');
-//   const bookshopLink = data.buy_links.find(link => link.name === 'Bookshop');
-//   const appleBooksLink = data.buy_links.find(
-//     link => link.name === 'Apple Books'
-//   );
-
-//   const bookElMarkup = `
-//     <img class="modal-card_img" src="${data.book_image}" alt="${data.title}" />
-//     <h3 class="modal-card_title">${data.title}</h3>
-//     <p class="modal-card_author">${data.author}</p>
-//     <p class="modal-card_desq">${data.description}</p>
-//     <ul class="shopping-list-links">
-//       <li><a href="${amazonLink.url}">${amazonLink.name}</a></li>
-//       <li><a href="${bookshopLink.url}">${bookshopLink.name}</a></li>
-//       <li><a href="${appleBooksLink.url}">${appleBooksLink.name}</a></li>
-//     </ul>
-//     <button class="button-add-shopping-list" type="button">Add to Shopping List</button>
-
-//   `;
-
-//   refs.modalCard.insertAdjacentHTML('beforeend', bookElMarkup);
-// };
-
-// const URL_API = 'https://books-backend.p.goit.global/books/';
-// // const bookId = '643282b1e85766588626a0dc';
-// const bookId = '643282b1e85766588626a085';
-
-// function fetchBook() {
-//   axios
-//     .get(`${URL_API}${bookId}`)
-//     .then(response => {
-//       const data = response.data;
-//       renderBooks(data, refs);
-//       console.log({ data });
-//     })
-//     .catch(error => {
-//       console.log('Error:', error);
-//     });
-// }
-
 import axios from 'axios';
 import { BookAPI } from './booksApi';
 
@@ -145,21 +64,36 @@ const renderBooks = (data, refs) => {
   const bookElMarkup = `
   <div class="modal-card-div">
   <img class="modal-card_img" src="${book.book_image}" alt="${book.title}" />
+  <div class="modal-card-info">
+
   <h3 class="modal-card_title">${book.title}</h3>
   <p class="modal-card_author">${book.author}</p>
   <p class="modal-card_desq">${book.description}</p>
   <ul class="shopping-list-links"> 
-    <li><a href="${amazonLink.url}">${amazonLink.name}</a>
-    <img class="img-amazon" src="./images/modal-card-amazon.png" alt=""></li>
+  <li>
+  <a href="${amazonLink.url}">
+    ${amazonLink.name}
+    <img class="img-amazon" src="./images/modal-card-icon.png" alt="">
+  </a>
+</li>
     <li><a href="${bookshopLink.url}">${bookshopLink.name}</a></li>
     <li><a href="${appleBooksLink.url}">${appleBooksLink.name}</a></li>
   </ul>
-  <button class="button-add-shopping-list btn-modal-card" type="button">Add to Shopping List</button>
+  </div>
+  </div>
+  <div class="button-shopping">
+  <button class="button-add-shopping-list btn-modal-card" type="button">Add to Shopping List</button> 
   </div>
   `;
 
   refs.modalCard.insertAdjacentHTML('beforeend', bookElMarkup);
 };
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    closeModalCard();
+  }
+});
 
 // function fetchBook() {
 //   axios
@@ -172,4 +106,25 @@ const renderBooks = (data, refs) => {
 //     .catch(error => {
 //       console.log('Error:', error);
 //     });
+// }
+
+// const refs = {
+//   openModalCardBtn: document.querySelector('button[name="data-modal-card-open"]'),
+//   modalCardBackDrop: document.querySelector('.modal-card_backdrop'),
+//   modalCard: document.querySelector('.modal-card'),
+//   closeModalCardBtn: document.querySelector('.modal-card_close'),
+// };
+
+// function openModalCard() {
+//   if (!isModalOpen) {
+//     refs.modalCardBackDrop.style.display = 'block';
+//     refs.modalCard.style.display = 'block';
+//     const data = bookApi
+//       .fetchBook(bookId)
+//       .then((data) => renderBooks(data, refs))
+//       .catch((e) => console.log(e));
+
+//     isModalOpen = true;
+//     refs.openModalCardBtn.style.display = 'none';
+//   }
 // }
