972ca3f35af243d088f98e465a41cc821c7568ab
IgorJS-Voloshyn <igorVoloshyn.js@gmail.com>
2023-05-15 22:27:04 +0300

    fixing margin

--- src/js/best-selling-books.js
+++ src/js/best-selling-books.js
@@ -27,7 +27,6 @@ async function fetchBestSellers() {
   const BASE_URL = 'https://books-backend.p.goit.global/';
   const END_POINT = `books/top-books`;
   const resp = await axios.get(`https://books-backend.p.goit.global/books/top-books`).then(response => response.data);
-  console.log(resp);
   return resp;
 }
 // fetchBestSellers()
@@ -40,15 +39,16 @@ fetchBestSellers()
     addClickListeners();
   })
   .catch(err => console.log(err));
+  
 // Viktoriia added //
 
 function createMarkupBooksCategories(arr) {
   return arr.map(({ list_name, books }) =>  
   `<div class="books_list_category">
-        <p class="category_name">${list_name}</p><ul class="books_row">${books.slice(0,booksPerList).map(({ book_image, title, author, _id, }) => ` <a href="#" class="modal_popap" target="_self">
-          <div class="book-card">
-
-              <div class="book-card__img-box">
+        <p class="category_name">${list_name}</p><ul class="books_row">${books.slice(0, booksPerList).map(({ book_image, title, author, _id, }) =>
+         ` <div class="book-card">
+           <a href="#" class="modal_popap" target="_self">
+          <div class="book-card__img-box">
                 <img class="book-card__img"src="${book_image}" alt="${title}" loading="lazy />
               </div>
               <div class="info ">
@@ -56,8 +56,7 @@ function createMarkupBooksCategories(arr) {
                   <p class="info-author__item">${author}</p>
                   <p class = "visually-hidden">${_id}</p>
               </div>
-          </div>
-      </a>`).join('')}</ul><button class="books-category-btn">see more</button>
+          </a></div>`).join('')}</ul><button class="books-category-btn">see more</button>
       </div> 
     `
   ).join('');
@@ -75,7 +74,15 @@ function addClickListeners() {
 }
 // Viktoriia added //
 
-export {fetchBestSellers };
+export { fetchBestSellers };
+  
+bestSellersGal.addEventListener('click', handleCategoryBtnClick);
+
+function  handleCategoryBtnClick(evt) {
+  const btnCategory = evt.currentTarget.classList.contains('${}');
+  console.log(btnCategory);
+  
+}
 
 //  return bestSellersGal.innerHTML = markupBooksList(data).join('');
 // };
@@ -83,10 +90,7 @@ export {fetchBestSellers };
 // //  fetchBestSellers().then(data => {
 // //    console.log(data);
 // // })
-// //   .catch(error => {
-// //        console.log(error);
-// //   Notiflix.Notify.failure('Oops, there is error');
-// //   });
+// //   
 
 // function markupBooksList(data) {
 //   console.log(data);
