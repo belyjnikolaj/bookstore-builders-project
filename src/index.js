b41798ec4da435bfd7c5903a610bf48408ac7061
Hanna <malahovaa79@gmail.com>
2023-05-16 16:02:24 +0300

    edit login form menu

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import { fetchBestSellers } from './js/best-selling-books';
 import { supportUkraine } from './js/support-ukraine';
 
-import { header } from './js/header';
-
 import { modalCard } from './js/modal-card';
 import { fetchBestSellers } from './js/best-selling-books';
 import { toggleModal } from './js/modal-registration';
---
+++ src/js/open-menu-registration.js
@@ -0,0 +1,30 @@
+const menuLoginBtn = document.querySelector('#js-open-menu-btn');
+
+console.log(menuLoginBtn);
+// const openMenuBtn = document.querySelector('#js-open-menu-btn');
+// const mobileMenu = document.querySelector('.js-menu-conteiner');
+// const closeMenuBtn = document.querySelector('.js-close-menu');
+
+// openMenuBtn.addEventListener('click', onToggleOpenBurgerBtn);
+// closeMenuBtn.addEventListener('click', onToggleOpenBurgerBtn);
+// console.log(mobileMenu);
+
+// function onToggleOpenBurgerBtn() {
+//   const isMenuOpen =
+//     openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+//   openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+//   mobileMenu.classList.toggle('is-open');
+// }
+
+// const scrollLockMethod = !isMenuOpen ? 'disableBodyScroll' : 'enableBodyScroll';
+// bodyScrollLock[scrollLockMethod](document.body);
+
+// console.log(scrollLockMethod);
+
+// // // Close the mobile menu on wider screens if the device orientation changes
+// // window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+// //   if (!e.matches) return;
+// //   mobileMenu.classList.remove('is-open');
+// //   openMenuBtn.setAttribute('aria-expanded', false);
+// //   bodyScrollLock.enableBodyScroll(document.body);
+// // });
