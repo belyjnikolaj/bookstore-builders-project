9e280579b802d2d657205935dc3ece73fcc654c6
vitaliyzeykan <112661803+vitaliyzeykan@users.noreply.github.com>
2023-05-17 00:36:54 +0300

    change dark function

--- src/js/dark.js
+++ src/js/dark.js
@@ -1,25 +1,3 @@
 export function darkMode() {
-  document.body.classList.toggle('dark-mode');
-  document
-    .querySelector('.header-conteiner')
-    .classList.toggle('dark-mode-header');
-  document.querySelector('.link').classList.toggle('link-dark');
-  document.querySelector('.nav-shopping-page').classList.toggle('link-dark');
-  document
-    .querySelector('.nav-shopping-page__icon')
-    .classList.toggle('icon-dark');
-  document.querySelectorAll('.books-category-btn').forEach(element => {
-    element.classList.toggle('category-btn-dark');
-  });
-
-  document.querySelectorAll('.info-title__item').forEach(element => {
-    element.classList.toggle('dark-title');
-  });
-  document.querySelectorAll('.list_name').forEach(element => {
-    element.classList.toggle('list-name-dark');
-  });
+  document.querySelector('html').classList.toggle('dark');
 }
-
-// const link = document.querySelectorAll('.link').forEach(element => {
-//   element.classList.toggle('link-dark');
-// });
