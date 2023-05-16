c8e807767a12dcee7f79ba54f72297bc7c20eed3
vitaliyzeykan <112661803+vitaliyzeykan@users.noreply.github.com>
2023-05-17 01:39:07 +0300

    add save theme to localstorage

--- src/js/dark.js
+++ src/js/dark.js
@@ -1,3 +1,29 @@
 export function darkMode() {
-  document.querySelector('html').classList.toggle('dark');
+  if (localStorage.getItem('theme') === 'dark') {
+    localStorage.removeItem('theme');
+  } else {
+    localStorage.setItem('theme', 'dark');
+  }
+  addDarkClass();
+  // document.querySelector('html').classList.toggle('dark');
 }
+
+function addDarkClass() {
+  try {
+    if (localStorage.getItem('theme') === 'dark') {
+      document.querySelector('html').classList.add('dark');
+      // ----- функція зміни іконки
+    } else {
+      document.querySelector('html').classList.remove('dark');
+      // ----функція зміни іконки
+    }
+  } catch (err) {}
+}
+addDarkClass();
+// document.querySelector('.js-color-switcher').addEventListener('click', () => {
+//   if (localStorage.getItem('theme') === 'dark') {
+//     localStorage.removeItem('theme');
+//   } else {
+//     localStorage.setItem('theme', 'dark');
+//   }
+// });
