dd9df260119eaf4beaedfc83ac089516347929d4
vitaliyzeykan <112661803+vitaliyzeykan@users.noreply.github.com>
2023-05-16 11:05:55 +0300

    add dark style book card NOT ALL

--- src/js/dark.js
+++ src/js/dark.js
@@ -5,5 +5,11 @@ export function darkMode() {
     .classList.toggle('dark-mode-header');
   document.querySelector('.link').classList.toggle('link-dark');
   document.querySelector('.nav-shopping-page').classList.toggle('link-dark');
-  //   document.querySelector('.navigation').classList.toggle('icon-dark');
+  document
+    .querySelector('.nav-shopping-page__icon')
+    .classList.toggle('icon-dark');
+  document
+    .querySelector('.books-category-btn')
+    .classList.toggle('category-btn-dark');
+  document.querySelector('.info-title__item').classList.toggle('dark-title');
 }
