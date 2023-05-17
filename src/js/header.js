a525b586b0902bd19af975e41e29fc2848467cfe
vitaliyzeykan <112661803+vitaliyzeykan@users.noreply.github.com>
2023-05-17 11:32:47 +0300

    add function saving value of button switcher

--- src/js/header.js
+++ src/js/header.js
@@ -2,14 +2,8 @@ import { darkMode } from './dark';
 
 const colorSwitcher = document.querySelector('.js-color-switcher');
 
-// console.log(colorSwitcher);
-
 colorSwitcher.addEventListener('click', darkMode);
-// if (colorSwitcher.checked) {
-//   alert('Вибраний');
-// } else {
-//   alert('не вибраний');
-// }
+
 function onClick(e) {
   //     let menu = document.querySelector('.menu');
   // menuBtn.addEventListener('click', function(){
